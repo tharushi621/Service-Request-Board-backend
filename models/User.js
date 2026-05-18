1e036f614761e80f3dccc29afc77b95a9c2a5eb5
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-05-19 04:09:05 +0530

    update User.js

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxlength: [60, "Name cannot exceed 60 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+      select: false, // never returned in queries by default
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare plain password to hashed
+userSchema.methods.matchPassword = async function (plain) {
+  return bcrypt.compare(plain, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
